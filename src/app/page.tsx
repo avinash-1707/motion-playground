20801c713b0902bf1cb00fee6c45538c021fab25
Avinash <avinashnarwariya5@gmail.com>
2025-05-31 06:34:17 +0530

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import MotionButton from "@/components/button";
+import Playground from "@/components/playground";
 
 export default function Home() {
-  return <MotionButton />;
+  return <Playground />;
 }
---
+++ src/components/playground.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import MotionButton from "./button";
+
+const Playground = () => {
+  return (
+    <div>
+      <MotionButton />
+    </div>
+  );
+};
+
+export default Playground;
